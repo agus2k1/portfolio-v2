2fe2c7eadebc0559a754fc32f51160a898ff62f3
West <agus2kcoin@outlook.com>
2023-01-16 20:45:05 -0300

    Add rotation animation on mouse move

--- Experience/World/Room.js
+++ Experience/World/Room.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import Experience from '../Experience';
+import GSAP from 'gsap';
 
 export default class Room {
   constructor() {
@@ -10,8 +11,15 @@ export default class Room {
     this.room = this.resources.items.room;
     this.actualRoom = this.room.scene;
 
+    this.lerp = {
+      current: 0,
+      target: 0,
+      ease: 0.1,
+    };
+
     this.setModel();
     this.setAnimation();
+    this.onMouseMove();
   }
 
   setModel() {
@@ -43,6 +51,7 @@ export default class Room {
     });
 
     this.scene.add(this.actualRoom);
+    this.actualRoom.scale.set(0.3, 0.3, 0.3);
   }
 
   setAnimation() {
@@ -53,13 +62,27 @@ export default class Room {
     // Black/White Fish
     this.fishAnimation2 = this.mixer.clipAction(this.room.animations[41]);
     this.fishAnimation2.play();
+  }
 
-    console.log(this.room);
+  onMouseMove() {
+    window.addEventListener('mousemove', (e) => {
+      this.rotation =
+        ((e.clientX - window.innerWidth / 2) * 2) / window.innerWidth; // From -1 to 1
+      this.lerp.target = this.rotation * 0.1;
+    });
   }
 
   resize() {}
 
   update() {
+    this.lerp.current = GSAP.utils.interpolate(
+      this.lerp.current,
+      this.lerp.target,
+      this.lerp.ease
+    );
+
+    this.actualRoom.rotation.y = this.lerp.current;
+
     this.mixer.update(this.time.delta * 0.001);
   }
 }
