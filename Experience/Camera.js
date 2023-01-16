96a96ed85b4467d47c254f0391e82ca168346b2a
West <agus2kcoin@outlook.com>
2023-01-16 20:39:39 -0300

    Set camera position

--- Experience/Camera.js
+++ Experience/Camera.js
@@ -22,37 +22,46 @@ export default class Camera {
       1000
     );
     this.scene.add(this.perspectiveCamera);
-    this.perspectiveCamera.position.z = 5;
+    this.perspectiveCamera.position.x = 29;
+    this.perspectiveCamera.position.y = 14;
+    this.perspectiveCamera.position.z = 12;
   }
 
   createOrthographicCamera() {
-    this.frustrum = 5;
     this.orthographicCamera = new THREE.OrthographicCamera(
       (-this.sizes.aspect * this.sizes.frustrum) / 2,
       (this.sizes.aspect * this.sizes.frustrum) / 2,
       this.sizes.frustrum / 2,
       -this.sizes.frustrum / 2,
-      -100,
-      100
+      -10,
+      10
     );
+
+    this.orthographicCamera.position.y = 4;
+    this.orthographicCamera.position.z = 5;
+    this.orthographicCamera.rotation.x = -Math.PI / 6;
 
     this.scene.add(this.orthographicCamera);
 
+    // Camera Helper
+    // this.helper = new THREE.CameraHelper(this.orthographicCamera);
+    // this.scene.add(this.helper);
+
     // Grid helper
-    const size = 20;
-    const divisions = 20;
-    const gridHelper = new THREE.GridHelper(size, divisions);
-    this.scene.add(gridHelper);
+    // const size = 20;
+    // const divisions = 20;
+    // const gridHelper = new THREE.GridHelper(size, divisions);
+    // this.scene.add(gridHelper);
 
     // Axis helper
-    const axesHelper = new THREE.AxesHelper(10);
-    this.scene.add(axesHelper);
+    // const axesHelper = new THREE.AxesHelper(10);
+    // this.scene.add(axesHelper);
   }
 
   setOrbitControls() {
     this.controls = new OrbitControls(this.perspectiveCamera, this.canvas);
     this.controls.enableDamping = true;
-    this.controls.enableZoom = true;
+    this.controls.enableZoom = false;
   }
 
   resize() {
@@ -72,5 +81,10 @@ export default class Camera {
 
   update() {
     this.controls.update();
+
+    // this.helper.matrixWorldNeedsUpdate = true;
+    // this.helper.update();
+    // this.helper.position.copy(this.orthographicCamera.position);
+    // this.helper.rotation.copy(this.orthographicCamera.rotation);
   }
 }
