a26a13edfa5700e103bff7d9791cacac73410cd1
West <agus2kcoin@outlook.com>
2023-01-17 20:43:41 -0300

    Add switch theme functionality

--- Experience/World/World.js
+++ Experience/World/World.js
@@ -1,8 +1,8 @@
 import * as THREE from 'three';
 import Experience from '../Experience';
-
-import Room from './Room';
 import Environment from './Environment';
+import Room from './Room';
+import Floor from './Floor';
 import Controls from './Controls';
 
 export default class World {
@@ -13,12 +13,24 @@ export default class World {
     this.canvas = this.experience.canvas;
     this.camera = this.experience.camera;
     this.resources = this.experience.resources;
+    this.theme = this.experience.theme;
 
     this.resources.on('ready', () => {
       this.environment = new Environment();
       this.room = new Room();
+      this.floor = new Floor();
       this.controls = new Controls();
     });
+
+    this.theme.on('switch', (theme) => {
+      this.switchTheme(theme);
+    });
+  }
+
+  switchTheme(theme) {
+    if (this.environment) {
+      this.environment.switchTheme(theme);
+    }
   }
 
   resize() {}
