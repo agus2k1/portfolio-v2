f795d4c97cc99c71d8e1bb159d51a5ff806e3052
West <agus2kcoin@outlook.com>
2023-01-19 17:25:51 -0300

    Fix scrolltrigger animations on resize

--- Experience/World/Controls.js
+++ Experience/World/Controls.js
@@ -25,145 +25,99 @@ export default class Controls {
   setScrollTrigger() {
     let mm = gsap.matchMedia();
 
-    // Desktop Version
-    mm.add('(min-width: 969px)', () => {
-      // Resets
-      this.room.scale.set(0.27, 0.27, 0.27);
-      this.room.position.set(0, 0, 0);
-      this.fishtankLight.width = 0.8;
-      this.fishtankLight.height = 0.5;
-
-      // First section
-      this.firstMoveTimeline = new gsap.timeline({
-        scrollTrigger: {
-          trigger: '.first-move',
-          start: 'top top',
-          end: 'bottom bottom',
-          scrub: 0.6,
-          invalidateOnRefresh: true,
-        },
-      }).to(this.room.position, {
-        x: () => this.sizes.width * 0.002,
-      });
-
-      // Second section
-      this.secondMoveTimeline = new gsap.timeline({
-        scrollTrigger: {
-          trigger: '.second-move',
-          start: 'top top',
-          end: 'bottom bottom',
-          scrub: 0.6,
-          invalidateOnRefresh: true,
-        },
-      })
-        .to(
-          this.room.position,
-          {
-            x: () => 1,
-            z: () => this.sizes.height * 0.0032,
-          },
-          'same'
-        )
-        .to(
-          this.room.scale,
-          {
-            x: () => 0.92,
-            y: () => 0.92,
-            z: () => 0.92,
-          },
-          'same'
-        )
-        .to(
-          this.fishtankLight,
-          {
-            width: 0.8 * 4,
-            height: 0.5 * 4,
-          },
-          'same'
-        );
-    });
-
-    // Third section
-    this.thirdMoveTimeline = new gsap.timeline({
-      scrollTrigger: {
-        trigger: '.third-move',
-        start: 'top top',
-        end: 'bottom bottom',
-        scrub: 0.6,
-        invalidateOnRefresh: true,
+    mm.add(
+      {
+        isDesktop: `(min-width: 969px)`,
+        isMobile: `(max-width: 968px)`,
       },
-    }).to(this.camera.orthographicCamera.position, {
-      x: -4.1,
-      y: -1,
-    });
+      (context) => {
+        let { isDesktop, isMobile } = context.conditions;
 
-    // Mobile Version
-    mm.add('(max-width: 968px)', () => {
-      // Resets
-      this.room.scale.set(0.2, 0.2, 0.2);
-      this.room.position.set(0, 0, 0);
+        // Resets
+        if (isDesktop) {
+          this.room.scale.set(0.27, 0.27, 0.27);
+          this.room.position.set(0, 0, 0);
+          this.fishtankLight.width = 0.8;
+          this.fishtankLight.height = 0.5;
+        } else if (isMobile) {
+          this.room.scale.set(0.2, 0.2, 0.2);
+          this.room.position.set(0, 0, 0);
+        }
 
-      // First section
-      this.firstMoveTimeline = new gsap.timeline({
-        scrollTrigger: {
-          trigger: '.first-move',
-          start: 'top top',
-          end: 'bottom bottom',
-          scrub: 0.6,
-          invalidateOnRefresh: true,
-        },
-      }).to(this.room.scale, {
-        x: 0.23,
-        y: 0.23,
-        z: 0.23,
-      });
+        // First Move Modifiers
+        let firstTargets = isDesktop ? this.room.position : this.room.scale;
+        let firstChanges = {
+          x: isDesktop ? () => this.sizes.width * 0.002 : 0.23,
+          y: isDesktop ? '' : 0.23,
+          z: isDesktop ? '' : 0.23,
+        };
 
-      // Second section
-      this.secondMoveTimeline = new gsap.timeline({
-        scrollTrigger: {
-          trigger: '.second-move',
-          start: 'top top',
-          end: 'bottom bottom',
-          scrub: 0.6,
-          invalidateOnRefresh: true,
-        },
-      })
-        .to(
-          this.room.scale,
-          {
-            x: 0.8,
-            y: 0.8,
-            z: 0.8,
-          },
-          'same'
-        )
-        .to(
+        // Second Move Modifiers
+        let secondTargets = [
           this.room.position,
+          this.room.scale,
+          this.fishtankLight,
+        ];
+        let secondChanges = [
           {
-            x: () => 3,
+            x: () => (isDesktop ? 1 : 3),
             z: () => this.sizes.height * 0.0032,
           },
-          'same'
-        );
+          {
+            x: () => (isDesktop ? 0.92 : 0.8),
+            y: () => (isDesktop ? 0.92 : 0.8),
+            z: () => (isDesktop ? 0.92 : 0.8),
+          },
+          {
+            width: () => (isDesktop ? 0.8 * 4 : 0.8 * 3),
+            height: () => (isDesktop ? 0.5 * 4 : 0.5 * 3),
+          },
+        ];
 
-      // Third section
-      this.thirdMoveTimeline = new gsap.timeline({
-        scrollTrigger: {
-          trigger: '.third-move',
-          start: 'top top',
-          end: 'bottom bottom',
-          scrub: 0.6,
-          invalidateOnRefresh: true,
-        },
-      }).to(
-        this.camera.orthographicCamera.position,
-        {
-          x: 1,
-          y: -1,
-        },
-        'same'
-      );
-    });
+        // Third Move Modifiers
+        let thirdTargets = this.camera.orthographicCamera.position;
+        let thirdChanges = {
+          x: () => (isDesktop ? -4.1 : 1),
+          y: () => (isDesktop ? -1 : -1),
+        };
+
+        // First section
+        this.firstMoveTimeline = new gsap.timeline({
+          scrollTrigger: {
+            trigger: '.first-move',
+            start: 'top top',
+            end: 'bottom bottom',
+            scrub: 0.6,
+            invalidateOnRefresh: true,
+          },
+        }).to(firstTargets, firstChanges);
+
+        // Second section
+        this.secondMoveTimeline = new gsap.timeline({
+          scrollTrigger: {
+            trigger: '.second-move',
+            start: 'top top',
+            end: 'bottom bottom',
+            scrub: 0.6,
+            invalidateOnRefresh: true,
+          },
+        })
+          .to(secondTargets[0], secondChanges[0], 'same')
+          .to(secondTargets[1], secondChanges[1], 'same')
+          .to(secondTargets[2], secondChanges[2], 'same');
+
+        // Third section
+        this.thirdMoveTimeline = new gsap.timeline({
+          scrollTrigger: {
+            trigger: '.third-move',
+            start: 'top top',
+            end: 'bottom bottom',
+            scrub: 0.6,
+            invalidateOnRefresh: true,
+          },
+        }).to(thirdTargets, thirdChanges);
+      }
+    );
   }
 
   resize() {}
