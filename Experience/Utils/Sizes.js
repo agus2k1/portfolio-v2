044975e7d5d94bac4f5d5fb4a9a975b46d701f7f
West <agus2kcoin@outlook.com>
2023-01-22 19:17:01 -0300

    Add device types

--- Experience/Utils/Sizes.js
+++ Experience/Utils/Sizes.js
@@ -8,6 +8,11 @@ export default class Sizes extends EventEmitter {
     this.aspect = this.width / this.height;
     this.pixelRatio = Math.min(window.devicePixelRatio, 2);
     this.frustrum = 5;
+    if (this.width < 968) {
+      this.device = 'mobile';
+    } else {
+      this.device = 'desktop';
+    }
 
     window.addEventListener('resize', () => {
       this.width = window.innerWidth;
